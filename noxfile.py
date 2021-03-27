8143b8ceade33ef75646edc0e81afd11fb0a2768
Sacha Wharton <sachajw@gmail.com>
2021-03-27 17:58:44 +0200

    hacking with nox

--- noxfile.py
+++ noxfile.py
@@ -5,10 +5,7 @@ import nox
 def test(session):
     session.install("-r", "requirements.txt")
     """Run the test suite"""
-#    session.run("pytest", "app/bubble-sort.py")
-    """Run the coverage suite"""
-    #session.run("coverage run -m pytest")
-    session.run("coverage report")
+    session.run("pytest","app/bubble-sort.py", "--collect-only")
 
 
 @nox.session(python="3.8")
@@ -16,4 +13,10 @@ def lint(session):
     session.install("-r", "requirements.txt")
     """Run the lint suite"""
     session.run("flake8", "app/bubble-sort.py")
-    session.run("black", "app/bubble-sort.py", "--check")
+
+
+@nox.session(python=["3.8"])
+def coverage(session):
+    """Run the coverage suite"""
+    session.run("coverage", "run", "-m", "pytest")
+    session.run("coverage", "report")
