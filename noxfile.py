3b8538ce6424723d9373f2def3e304609f43b76b
Sacha Wharton <sachajw@gmail.com>
2021-03-27 19:51:59 +0200

    test configuration

--- noxfile.py
+++ noxfile.py
@@ -3,20 +3,21 @@ import nox
 
 @nox.session(python=["3.8"])
 def test(session):
-    session.install("-r", "requirements.txt")
     """Run the test suite"""
+    session.install("-r", "requirements.txt")
     session.run("pytest","app/bubble-sort.py", "--collect-only")
 
 
 @nox.session(python="3.8")
 def lint(session):
-    session.install("-r", "requirements.txt")
     """Run the lint suite"""
+    session.install("-r", "requirements.txt")
     session.run("flake8", "app/bubble-sort.py")
 
 
 @nox.session(python=["3.8"])
 def coverage(session):
     """Run the coverage suite"""
+    session.install("-r", "requirements.txt")
     session.run("coverage", "run", "-m", "pytest")
     session.run("coverage", "report")
