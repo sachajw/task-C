75a16f6c21ed9a111d05af753b44decfa674ad74
Sacha Wharton <sachajw@gmail.com>
2021-03-27 17:07:02 +0200

    added ignores for flake8 and gitignore

--- app/bubble-sort.py
+++ app/bubble-sort.py
@@ -12,19 +12,16 @@ app = Flask(__name__)
 
 def bubbleSort():
     n = len(arr)
-   
     # Traverse through all array elements
     for i in range(n):
         swapped = False
-  
         # Last i elements are already in place
         for j in range(0, n-i-1):
-
             # traverse the array from 0 to
             # n-i-1. Swap if the element 
             # found is greater than the
             # next element
-            if arr[j] > arr[j+1] :
+            if arr[j] > arr[j+1]:
                 arr[j], arr[j+1] = arr[j+1], arr[j]
                 swapped = True
         # If no two elements were swapped
@@ -36,7 +33,7 @@ def bubbleSort():
 # number sequence to test above
 arr = [66, 37, 23, 17, 27, 14, 95]
 
-   
+
 bubbleSort()
 
 
--- noxfile.py
+++ noxfile.py
@@ -5,13 +5,15 @@ import nox
 def test(session):
     session.install("-r", "requirements.txt")
     """Run the test suite"""
-    session.run("pytest")
+#    session.run("pytest", "app/bubble-sort.py")
+    """Run the coverage suite"""
+    #session.run("coverage run -m pytest")
+    session.run("coverage report")
 
 
 @nox.session(python="3.8")
 def lint(session):
     session.install("-r", "requirements.txt")
     """Run the lint suite"""
-#    session.run("flake8", "app/bubble-sort.py")
+    session.run("flake8", "app/bubble-sort.py")
     session.run("black", "app/bubble-sort.py", "--check")
-#    ignore = ("E117") #E203 E226 E302 E304 E305 E712 F401 W291 W293 
